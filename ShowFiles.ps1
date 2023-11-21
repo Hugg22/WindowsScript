7e3d60396d75ee02924f42692b67a967e2a7de68
Hugg22 <150303239+Hugg22@users.noreply.github.com>
2023-11-21 16:39:50 -0600

    ShowFiles.ps1

--- ShowFiles.ps1
+++ ShowFiles.ps1
@@ -1,2 +1,4 @@
-Get-ChildItem -Path C:\users\*.txt -Recurse -Force
-Get-ChildItem -Path C:\users\*.png -Recurse -Force
+#shows way to much information
+
+Get-ChildItem -Path C:\users\*.txt -Recurse -Force -Depth 2 > c:\Users\txtOutPut
+Get-ChildItem -Path C:\users\*.png -Recurse -Force -Depth 2 > c:\Users\pngOutPut
