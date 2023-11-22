50527733019ea570275ddd4b91c7a0bee3bca5de
Hugg22 <150303239+Hugg22@users.noreply.github.com>
2023-11-21 21:31:51 -0600

    Add files via upload

--- ShowFiles.ps1
+++ ShowFiles.ps1
@@ -1,4 +1,4 @@
 #shows way to much information
 
-Get-ChildItem -Path C:\users\*.txt -Recurse -Force -Depth 2 > c:\Users\txtOutPut
-Get-ChildItem -Path C:\users\*.png -Recurse -Force -Depth 2 > c:\Users\pngOutPut
+#Get-ChildItem -Path C:\users\*.txt -Recurse -Force 
+Get-ChildItem -Path C:\users\*.png  -Recurse -Force -Depth 2 > c:\Users\pngOutPut
